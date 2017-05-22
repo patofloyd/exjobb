46c2f4414a6009fd13f1614821d42e64b310f623
Patricio Vergara Paredes <pato.vergara.p@gmail.com>
2017-05-22 12:05:10 +0200

    Cleaning and commenting the some lines of code

--- site_node_angular/classes/REST.class.js
+++ site_node_angular/classes/REST.class.js
@@ -5,8 +5,7 @@ module.exports = class REST {
     this.settings = s.REST;
     this.DrupalData = s.DrupalData;
     this.app = express;
-    this.GET();
-  
+    this.GET(); 
   }
 
   GET(){
@@ -28,7 +27,7 @@ module.exports = class REST {
       if (isErr.length == 0) {
         res.sendStatus(404);
         res.end();
-        return;
+        //return;
       }
       else{
         m.request({
@@ -36,30 +35,25 @@ module.exports = class REST {
           json: true
         }, function(error, response, body){
           if (error) {
-            console.log("ERROR WITH THE DATA");
+            console.log("ERROR! SOMETHING WENT WRONG!");
           }
           else{
             if (!modelID) {
               var obj = {};
               var arr = [];
-              if (model == 'albums') {
+              if (model == 'albums') {  // Creating the new object using the album help function if the model is "albums".
                 body.forEach(function(x){
                   me.createAlbumObj(x, obj);
                   arr.push(obj);
-                  arr.push(obj);
-                  arr.push(obj);
+                  arr.push(obj); // DUMMYDATA
+                  arr.push(obj); // DUMMYDATA
                   obj = {};
                 });
-              }
-              else if (model == 'musicstyles') {
-                for (var i = 0; i < body.length; i++) {
-                  arr.push({title: body[i].title, name: body[i].title.split(" ")[0]});
-                }
               }
               else{
                 for (var i = 0; i < body.length; i++) {
                   if (body[i].field_image) {
-                    body[i].image = me.getImage(body[i].field_image);
+                    body[i].image = me.getImage(body[i].field_image); // Calling the function that creates the image link if there are images.
                   }
                   arr.push(body[i]);
                 }
@@ -67,12 +61,10 @@ module.exports = class REST {
               res.json(arr);
             }
             else{
-              var isThere = [];
               var obj = {};
               var arr = [];
               for (var i = 0; i < body.length; i++) {
-                if (body[i].nid == modelID) {
-                  isThere.push(i);
+                if (body[i].nid == modelID) { // Getting one object depending on the id
                   var arr2 = [];
                   if (model == 'albums') {
                     me.createAlbumObj(body[i], obj);
@@ -85,25 +77,23 @@ module.exports = class REST {
                   res.json(arr2);
                   break;
                 }
+                else if (model == 'albums' && me.getArtistNameAndNid(body[i].field_album_artist).name.toLowerCase().indexOf(modelID.toLowerCase()) >= 0) {
+                  me.createAlbumObj(body[i], obj); // Getting the data with some filters.
+                  arr.push(obj);
+                  obj = {};
+                }
                 else if (model == 'albums' && body[i].field_album_music_style.toLowerCase().indexOf(modelID.toLowerCase()) >= 0) {
-                  isThere.push(i);
-                  me.createAlbumObj(body[i], obj);
+                  me.createAlbumObj(body[i], obj); // Getting the data with some filters.
                   arr.push(obj);
                   obj = {};
                 }
                 else if (model == 'albums' && body[i].field_album_type.toLowerCase().indexOf(modelID.toLowerCase()) >= 0) {
-                  isThere.push(i);
-                  me.createAlbumObj(body[i], obj);
+                  me.createAlbumObj(body[i], obj); // Getting the data with some filters.
                   arr.push(obj);
                   obj = {};
-                }
-                else if (model == 'albums' && body[i].field_album_artist.toLowerCase().indexOf(modelID.toLowerCase()) >= 0) {
-                  isThere.push(i);
-                  me.createAlbumObj(body[i], obj);
-                  arr.push(obj);
-                  obj = {};
-                }
-                if (i == body.length - 1 && isThere.length == 0) {
+                }               
+                if (i == body.length - 1 && arr.length == 0) {
+                  console.log("The Object was not found");
                   res.json({text: "The Object was not found"});
                 }
               }
@@ -118,32 +108,31 @@ module.exports = class REST {
     });
   }
 
-  createAlbumObj(oldObj, newObj){
+  createAlbumObj(oldObj, newObj){ // Help function to create a better album object
     newObj.album_title = oldObj.title;
     newObj.field_album_artist = oldObj.field_album_artist;
     newObj.album_description = oldObj.field_album_description;
     newObj.album_price = oldObj.field_album_price;
     newObj.album_stock = oldObj.field_album_stock;
     newObj.album_style = oldObj.field_album_music_style;
-    newObj.album_style_name = oldObj.field_album_music_style.indexOf(" ") ? oldObj.field_album_music_style.split(" ")[0] : oldObj.field_album_music_style;
     newObj.album_type = oldObj.field_album_type;
     newObj.album_nid = oldObj.nid;
     newObj.album_image = this.getImage(oldObj.field_image);
     newObj.album_date_updated = oldObj.changed;
-
-    var splitField = newObj['field_album_artist'].split("/");
-    var splitFieldOfSplit = splitField[4].split('"');
-
-    newObj.album_artist_nid = splitFieldOfSplit[0];
-
-    var deleteSymbolOne = splitFieldOfSplit[3].substr(1);
-    var album_artist = deleteSymbolOne.slice(0,-1);
-
-    newObj.album_artist = album_artist;
-    
+    newObj.album_artist_nid = this.getArtistNameAndNid(newObj.field_album_artist).nid;
+    newObj.album_artist = this.getArtistNameAndNid(newObj.field_album_artist).name;    
   }
 
-  getImage(uri){
+  getArtistNameAndNid(link){  // Help function to get the album artist name and id
+    var splitField = link.split("/");
+    var splitFieldOfSplit = splitField[4].split('"');
+    var album_artist_nid = splitFieldOfSplit[0];
+    var deleteSymbolOne = splitFieldOfSplit[3].substr(1);
+    var album_artist = deleteSymbolOne.slice(0,-1);
+    return {name: album_artist, nid: album_artist_nid};
+  }
+
+  getImage(uri){  // Help function to get the correct image URL
     var splitUrl = uri.split('"');
     return splitUrl[1];
   }
@@ -152,4 +141,5 @@ module.exports = class REST {
     res.status(400);
     res.json(err);
   }
+
 };
